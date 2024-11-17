b24441dfeab59052c9ee1f31ebbaa9939476f38a
iAM7ander <iAM7ander@outlook.com>
2024-11-17 16:58:53 -0500

    update formatting

--- dice-roller-game/index.js
+++ dice-roller-game/index.js
@@ -11,7 +11,7 @@ function rollDice(){
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
-        images.push(`<img src="./dice_images/${value}.jpg" alt="Dice ${value}">`);
+        images.push(`<img src="./dice_one.jpg${value}.jpg" alt="Dice ${value}">`);
     }
 
     diceResult.textContent = `dice: ${values.join(', ')}`;
--- pokemon-fetch-api/index.js
+++ pokemon-fetch-api/index.js
@@ -1,5 +1,5 @@
 // fetch - function used for making HTTP requests to fetch resources.
-// json style data, images, files 
+// json style data, images, files
 // simplifies asynchronous data fetching in JavaScript and used for interacting with API's to retrieve and send data asynchronously over the internet
 // fetch(url, {method: "PUT", "POST", "DELETE"})
 
@@ -14,30 +14,27 @@
 //    .then(data => console.log(data))
 //    .catch(error => console.log(error));
 
+async function fetchData() {
+	try {
+		const pokemonName = document
+			.getElementById("pokemonName")
+			.value.toLowerCase();
 
+		const response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+		);
 
+		if (!response.ok) {
+			throw new Error("Could not fetch resources");
+		}
 
-async function fetchData(){
+		const data = await response.json();
+		const pokemonSprite = data.sprites.front_default;
+		const imgElement = document.getElementById("pokemonSprite");
 
-    try{
-
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-        if(!response.ok){
-            throw new Error("Could not fetch resources");
-        }
-
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite");
-
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block";
-    }
-    catch(error){
-        console.error(error);
-    }
-    
+		imgElement.src = pokemonSprite;
+		imgElement.style.display = "block";
+	} catch (error) {
+		console.error(error);
+	}
 }
